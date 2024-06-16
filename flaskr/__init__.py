485dea22ce8f047180ea585d58560130432d0b1f
h1ne <henryharlay@gmail.com>
2024-06-16 17:25:24 +0900

    Change module structure: to impliment web app by run.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,61 +6,63 @@ from flask import request
 
 STATUS = " "
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    # app.config.from_mapping(
-    #     SECRET_KEY='dev',
-    #     SECRET_KEY = os.urandom(24),
-    #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    # )
+app = Flask(__name__)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+# def create_app(test_config=None):
+#     # create and configure the app
+#     app = Flask(__name__, instance_relative_config=True)
+#     # app.config.from_mapping(
+#     #     SECRET_KEY='dev',
+#     #     SECRET_KEY = os.urandom(24),
+#     #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+#     # )
+#
+#     if test_config is None:
+#         # load the instance config, if it exists, when not testing
+#         app.config.from_pyfile('config.py', silent=True)
+#     else:
+#         # load the test config if passed in
+#         app.config.from_mapping(test_config)
+#
+#     # ensure the instance folder exists
+#     try:
+#         os.makedirs(app.instance_path)
+#     except OSError:
+#         pass
+
+@app.route('/')
+def mainmenu():
+    return render_template('mainmenu.html')
+
+@app.route('/materials', methods=['POST'])
+def materials():
+    STATUS = request.form['button'] # quiz or word_list
+    PREV_STATUS = request.form['prev-status']
+    # MATERIAL_STATUS = request.form['material_status']
+    print("STATUS           = " + STATUS)
+    print("PREV_STATUS      = " + PREV_STATUS)
+    # print("MATERIAL_STATUS  = " + MATERIAL_STATUS)
+    if PREV_STATUS == 'mainmenu':
+        return render_template('materials.html')
+    elif PREV_STATUS == 'materials':
+        if STATUS == quiz:
+            return render_template('quiz.html')
+        elif STATUS == word_list:
+            return render_template('wordlist.html')
     else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+        print('Unknown choice selected')
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+@app.route('/quiz', methods=['POST'])
+def quiz():
+    # MATERIAL_STATUS = request.from['material_status']
+    return render_template('quiz.html')
 
-    @app.route('/')
-    def mainmenu():
-        return render_template('mainmenu.html')
+@app.route('/wordlist', methods=['POST'])
+def wordlist():
+    return render_template('wordlist.html')
 
-    @app.route('/materials', methods=['POST'])
-    def materials():
-        STATUS = request.form['button'] # quiz or word_list
-        PREV_STATUS = request.form['prev-status']
-        # MATERIAL_STATUS = request.form['material_status']
-        print("STATUS           = " + STATUS)
-        print("PREV_STATUS      = " + PREV_STATUS)
-        # print("MATERIAL_STATUS  = " + MATERIAL_STATUS)
-        if PREV_STATUS == 'mainmenu':
-            return render_template('materials.html')
-        elif PREV_STATUS == 'materials':
-            if STATUS == quiz:
-                return render_template('quiz.html')
-            elif STATUS == word_list:
-                return render_template('wordlist.html')
-        else:
-            print('Unknown choice selected')
+# @app.route('back', methods=['POST'])
+# def back():
+#     PREV_STATUS = request.form['prev-status']
 
-    @app.route('/quiz', methods=['POST'])
-    def quiz():
-        # MATERIAL_STATUS = request.from['material_status']
-        return render_template('quiz.html')
-
-    @app.route('/wordlist', methods=['POST'])
-    def wordlist():
-        return render_template('wordlist.html')
-
-    # @app.route('back', methods=['POST'])
-    # def back():
-    #     PREV_STATUS = request.form['prev-status']
-
-    return app
+    # return app
